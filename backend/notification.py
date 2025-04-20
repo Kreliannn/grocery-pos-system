c1805d3c25846fad072666f46d18c999a8f4326c
Kreliannn <krelianquimson@gmail.com>
2025-04-20 14:25:25 +0800

    add notification class

---
+++ backend/notification.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from backend.db.database import Database
+
+class Notification(Database):
+
+    def addTransaction(self, notification):
+        current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        sql = "INSERT INTO notifications (header, message, icon, datetime) VALUES (%s, %s, %s, %s)"
+        values = (notification['header'], notification['message'], notification['icon'], current_datetime)
+        self.insert(sql, values)
+    
+    def getNotifications(self):
+        sql = "select * from notifications"
+        return list(reversed(self.getAll(sql)))
